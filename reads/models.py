d4491b8fcd6a5a9c8686d5aa3f5f58dc10b38fe7
Jason Bongiovanni <jpbongiovanni@gmail.com>
2020-12-24 07:13:57 +0900

    revisiting the dojoreads assignemnt

--- reads/models.py
+++ reads/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from registration_app.models import User
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    contributor = models.ForeignKey(User, related_name = "book_added", on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    book_written = models.OneToOneField(Book, on_delete = models.CASCADE, primary_key=True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+class Review(models.Model):
+    content = models.CharField(max_length=255)
+    rating = models.IntegerField()
+    poster = models.ForeignKey(User, related_name="user_reviews", on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, related_name="book_reviews", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
--- reads/views.py
+++ reads/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib import messages
+from .models import Book, Author, Review
+from registration_app.models import User
 
 def index(request):
-    return render(request, "books_home.html")
+    context = {
+        "user": User.objects.get(id = request.session['user_id']),
+        "book": Book.objects.all(),
+    }
+    return render(request, "books_home.html", context)
